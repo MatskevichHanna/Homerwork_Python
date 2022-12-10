e930b17ccef7663425a8b99f2d886c2ff19e2114
Matskevich_Hanna <hankohanna@mail.ru>
2022-12-11 01:21:50 +0300

    задача на расстояние между точками 2D

--- Desktop/PY/lesson01/task05.py
+++ Desktop/PY/lesson01/task05.py
@@ -4,3 +4,9 @@
 # - A (3,6); B (2,1) -> 5.09
 # - A (7,-5); B (1,-1) -> 7.21
 
+print('Введите координаты первой точки через пробел: ')
+num_X1, num_Y1 = map(int, input().split(' '))
+print('Введите координаты второй точки через пробел: ')
+num_X2, num_Y2 = map(int, input().split(' '))
+distance = float(((num_X2 - num_X1)** 2 + (num_Y2 - num_Y1)** 2) ** 0.5)
+print(int(distance * 100) / 100)
